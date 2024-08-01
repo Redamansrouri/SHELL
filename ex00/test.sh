570d2676f508c617470e13cfda1fbdf3c9a6d040
Redamansrouri <mnreda58@gmail.com>
2024-08-01 16:38:57 +0100

    (:

---
+++ ex00/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Hi"
+
+sleep 3
+
+echo "Hi"
+
+sleep 3
+
+echo "How Are You"
+
+sleep 3
+
+echo "Im Fine And You"
+
+sleep 3
+
+echo "Im good And Im Happy To See You"
+
+sleep 3
+
+echo "Me to"
